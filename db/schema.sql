e954c5535f2690af10ed00ad5f217588ac89e92e
mserafin20 <mmserafin20@gmail.com>
2023-06-19 22:13:33 -0400

    tested schema in workbench, made editis and copied back over

--- db/schema.sql
+++ db/schema.sql
@@ -8,20 +8,21 @@ CREATE TABLE department (
     name VARCHAR(30)
 );
 
-CREATE TABLE role(
+CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT.
+    department_id INT,
     FOREIGN KEY(department_id)
-    REFERENCE department(id)
+    REFERENCES department(id)
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCE role(id),
-    manager_id INT FOREIGN KEY (manager_id) REFERENCE employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT, 
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
