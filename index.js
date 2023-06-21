ab5bd597b1cb6a3826b608678424b444fc02bcd9
mserafin20 <mmserafin20@gmail.com>
2023-06-20 20:18:15 -0400

    finished last 2 functions for adding employees and updating the employee roles

--- index.js
+++ index.js
@@ -37,15 +37,15 @@ function mainMenu () {
     if(answer.choice == "Add a department") {
       addDepartment()
     }
-    // if(answer.choice == "View all departments") {
-    //   viewAllDepartments()
-    // }
-    // if(answer.choice == "View all departments") {
-    //   viewAllDepartments()
-    // }
-    // if(answer.choice == "View all departments") {
-    //   viewAllDepartments()
-    // }
+    if(answer.choice == "Add a role") {
+      addRole()
+    }
+    if(answer.choice == "Add an employee") {
+      addEmployee()
+    }
+    if(answer.choice == "Update an employee role") {
+      updateEmployeeRole()
+    }
     // if(answer.choice == "View all departments") {
     //   viewAllDepartments()
     // }
@@ -72,14 +72,14 @@ async function viewAllRoles() {
   mainMenu();
 };
 
-// function to view data in the employee table
+// function to view data in the employee table 
 async function viewAllEmployees() {
   const employeeData = await db.promise().query("SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN role r ON e.role_id = r.id JOIN department d ON d.id = r.department_id;")
   await showTable(employeeData[0]);
   mainMenu()
 };
 
-// function to add a department based on user input
+// function to add a department based on user input to the database
 async function addDepartment() {
   const response = await inquirer.prompt([
     {
@@ -91,90 +91,93 @@ async function addDepartment() {
   const departmentData = await db.promise().query("INSERT INTO department (name) VALUES (?)", [response.department_name])
 };
 
-// function to update an employee role using input from the user
-const updateEmployee = async function () {
-  console.log("test");
+// function to add a role based on user input to the database
+async function addRole() {
+  const departmentData = await db.promise().query("SELECT * FROM department")
+  const departmentChoices = departmentData[0].map(({ id, name }) => ({ name: name, value: id }))
+  const response = await inquirer.prompt([
+    {
+      type: "input",
+      name: "role_name",
+      message: "What is the new role called?"
+    },
+    {
+      type: "input",
+      name: "role_salary",
+      message: "What will the new salary of this role be?"
+    },
+    {
+      type: "list",
+      name: "role_id",
+      message: "Which department does this new role belong to?",
+      choices: departmentChoices
+    }
+  ]);
+
+  const newRole = await db.promise().query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [response.role_name, response.role_salary, response.role_id])
+  mainMenu();
+};
+
+// function to add an employee based on user input into the database
+
+async function addEmployee() {
+  const roleData = await db.promise().query("SELECT * FROM role");
+  const roleChoices = roleData[0].map(({ title, id }) => ({ name: title, value: id }));
+  const employeeData = await db.promise().query("SELECT * FROM employee");
+  const employeeChoices = employeeData[0].map(({ first_name, last_name, id }) => ({ name: `${first_name} ${last_name}`, value: id}));
+  const response = await inquirer.prompt([
+    {
+      type: "input",
+      name: "first_name",
+      message: "What is the new employee's first name?"
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "What is the new employee's last name?"
+    },
+    {
+      type: "list",
+      name: "role_id",
+      message: "What is the new employee's role?",
+      choices: roleChoices
+    },
+    {
+      type: "list",
+      name: "manager_id",
+      message: "Who will be the employee's manager?",
+      choices: employeeChoices
+    },
+  ]);
+
+  const newEmployee = await db.promise().query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [response.first_name, response.last_name, response.role_id, response.manager_id])
+  mainMenu();
+};
+
+// function to update an employee role using input from the user to the database
+async function updateEmployeeRole() {
+  const employeeData = await db.promise().query("SELECT * FROM employee");
+  const employeeChoices = employeeData[0].map(({ first_name, last_name, id }) => ({ name: `${first_name} ${last_name}`, value: id}));
+  const roleData = await db.promise().query("SELECT * FROM role");
+  const roleChoices = roleData[0].map(({ title, id }) => ({ name: title, value: id }));
+
+  const response = await inquirer.prompt([
+    {
+      type: "list",
+      name: "employee",
+      message: "Which employee's role needs to be updated?",
+      choices: employeeChoices
+    },
+    {
+      type: "list",
+      name: "employee_role",
+      message: "What role will be assigned to the selected employee?",
+      choices: roleChoices
+    }
+  ])
+
+  const updatedEmployee = await db.promise().query("UPDATE employee SET role_id=? WHERE id=?", [response.employee_role, response.employee])
+  mainMenu();
 }
-
-
-// const addCourse = async function() {
-//   //TODO complete adding a course by asking for instructor and course info
-//   console.log("Test");
-
-//   const results = await db.query("SELECT * FROM department");
-//   const dbData = results[0];
-//   const choiceData = dbData.map( (row) => ({
-//     name: row.first_name + " " + row.last_name,
-//     value: row
-//   }))
-//   choiceData.push({
-//     name: "No Instructor",
-//     value: {id: null}
-//   })
-//   await showTable([{JSON: JSON.stringify(dbData)}]);
-//   await showTable(dbData);
-  
-//   const firstAnswer = await inquirer.prompt([
-//     {
-//       message: "Which instructor will teach this course?",
-//       type: 'list',
-//       choices: choiceData,
-//       name: "instructor"
-//     }
-//   ]);
-//   // console.log(firstAnswer);
-//   // console.log(firstAnswer.instructor.id);
-
-//   const otherData = await inquirer.prompt([
-//     {
-//       message: "What is the course title?",
-//       type: 'input',
-//       name: "course_title"
-//     },
-//     {
-//       message: "What is the course description?",
-//       type: 'input',
-//       name: "course_description"
-//     },
-//     {
-//       message: "Is course active?",
-//       type: 'choice',
-//       choices: [0, 1],
-//       name: "active"
-//     },
-//   ]);
-//   otherData.date_added = "2023-06-06";
-//   otherData.instructor_id = firstAnswer.instructor.id;
-//   await showTable([otherData]);
-//   // console.log(otherData);
-//   await db.query("INSERT INTO courses SET ?", otherData);
-//   await menu();
-// };
-
-// const menu = async function () {
-//   // console.log("Do stuff after!");
-
-//   const answers = await inquirer.prompt([
-//     {
-//       message: "What do you want...",
-//       type: 'list',
-//       name: 'option',
-//       choices: [
-//         "Add a course"
-//       ]
-//     }
-//   ])
-//   if(answers.option === "Add a course"){
-//     addCourse();
-//   }
-// }
-
-// const init = async function(){
-//   // show table is an async function which will look like a promise in console
-//   // if we print
-//   await showTable();
-//   await mainMenu();
-// }
-
 mainMenu();
 
